abf39f18e2b6ee870103e3acab57a76c7ca4229f
MacBook <andredegbe@gmail.com>
2019-03-07 23:13:44 +0000

    bug fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { AboutComponent } from './about/about.component';
+//import { CoursesComponent } from './courses/courses.component';
+import { VisionComponent } from './vision/vision.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: AccueilComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'our/:id',
+    component: VisionComponent
+  },
+  /* {
+    path: 'programs',
+    component: CoursesComponent
+  } */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { VisionComponent } from './vision/vision.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    AboutComponent,
+    CoursesComponent,
+    HeaderComponent,
+    FooterComponent,
+    VisionComponent
   ],
   imports: [
     BrowserModule,
